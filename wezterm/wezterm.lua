c75b1dea28c6374cc6ed62e63bddf50994463cb7
Julian Lindsay-Kaufman <101271303+julianlk522@users.noreply.github.com>
2024-11-01 14:35:07 -0400

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,6 +8,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Cobalt Neon (Gogh)"
 config.font = wezterm.font("MartianMono Nerd Font")
 config.font_size = 10.0
+
 config.inactive_pane_hsb = {
 	saturation = 0.9,
 	brightness = 0.8,
@@ -18,9 +19,17 @@ config.window_background_image_hsb = {
 	hue = 1.0,
 	saturation = 0.1,
 }
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
+
 config.initial_rows = 35
 config.initial_cols = 150
 
--- return config to wezterm
+config.keys = {
+	{
+		key = "q",
+		mods = "CTRL",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	}
+}
+
 return config
