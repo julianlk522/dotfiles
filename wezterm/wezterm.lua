3ab4b327a78e701732190ffd790b9329c2256fd4
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2024-11-02 02:16:36 -0400

    minor wezterm config tidying, disable bg image by default

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,6 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- configs
 config.color_scheme = "Cobalt Neon (Gogh)"
 config.font = wezterm.font("MartianMono Nerd Font")
 config.font_size = 10.0
@@ -13,13 +9,13 @@ config.inactive_pane_hsb = {
 	saturation = 0.9,
 	brightness = 0.8,
 }
-config.window_background_image = "~/.config/wezterm/bg.jpg"
+-- config.window_background_image = "~/.config/wezterm/bg.jpg"
 config.window_background_image_hsb = {
 	brightness = 0.02,
 	hue = 1.0,
 	saturation = 0.1,
 }
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.85
 
 config.initial_rows = 35
 config.initial_cols = 150
@@ -29,7 +25,6 @@ config.keys = {
 		key = "q",
 		mods = "CTRL",
 		action = wezterm.action.CloseCurrentPane({ confirm = false }),
-	}
+	},
 }
-
 return config
